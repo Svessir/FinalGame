cd2620e78b25d9dc0e36e22515ca7d7ef102914f
Bollasupa <magnusvb14@ru.is>
2017-03-01 19:32:47 +0000

    sub control and gyro stuff

---
+++ Assets/Maggi/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform sub;
+
+    private void LateUpdate()
+    {
+        Vector3 newPos;
+        newPos.x = sub.position.x;
+        newPos.y = sub.position.y;
+        newPos.z = this.transform.position.z;
+        this.transform.position = newPos;
+
+    }
+}
