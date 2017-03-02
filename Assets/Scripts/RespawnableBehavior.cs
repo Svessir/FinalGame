fe0a68c8f77da3aada858e64b07d85004891e04c
sverrir <sverrir1993@gmail.com>
2017-03-02 22:43:32 +0000

    RespawnableBehaviors now subsribe to CheckpointReachedEvent. Any class that wants to save checkpoint variables on that event can override the subscription method 'OnCheckpointReached' for those purposes.

--- Assets/Scripts/RespawnableBehavior.cs
+++ Assets/Scripts/RespawnableBehavior.cs
@@ -2,7 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class RespawnableBehavior : MonoBehaviour {
+public abstract class RespawnableBehavior : MonoBehaviour 
+{
+	[SerializeField]
+	private CheckPointManager checkpointManager;
 
 	public abstract void Respawn (Vector3 respawnLocation);
+
+	void OnEnable() 
+	{
+		Subscribe ();
+	}
+
+	void OnDisable()
+	{
+		UnSubscribe ();
+	}
+
+	private void Subscribe()
+	{
+		if (checkpointManager != null)
+			checkpointManager.CheckpointReachedEvent += OnCheckpointReached;
+	}
+
+	private void UnSubscribe()
+	{
+		if (checkpointManager != null)
+			checkpointManager.CheckpointReachedEvent -= OnCheckpointReached;
+	}
+
+	/**
+	 * <summary>
+	 * Called when the checkpoint manager has reached a checkpoint.
+	 * Override this method to store any variables that need saving for that checkpoint.
+	 * </summary>
+	 * 
+	 * */
+	private void OnCheckpointReached(CheckPoint checkpoint) 
+	{
+	}
 }
