c10117b1d9016bb210c3861fb68433de0b643944
Drulludanni <gudni14@ru.is>
2017-03-11 23:04:01 +0000

    onar aggression on fish added

---
+++ Assets/ReorientFish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReorientFish : MonoBehaviour {
+    public float rotRate = 90;
+    float angle = 0;
+	// Update is called once per frame
+	void Update () {
+        if (transform.forward.x < 0) {
+            if (angle < 180) {
+                angle += rotRate * Time.deltaTime;
+                if (angle > 180) {
+                    angle = 180;
+                }
+            }
+        } else
+        {
+            if (angle > 0)
+            {
+                angle -= rotRate * Time.deltaTime;
+                if (angle < 0)
+                {
+                    angle = 0;
+                }
+            }
+        }
+        transform.rotation = transform.parent.rotation* Quaternion.Euler(new Vector3(0, 90, angle));
+
+    }
+}
