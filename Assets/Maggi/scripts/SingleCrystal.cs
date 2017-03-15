0d889b1c2be48a652d181ee1d96064494b0b8ac5
Bollasupa <magnusvb14@ru.is>
2017-03-15 12:55:56 +0000

    modular light, light interaction interfaces added

---
+++ Assets/Maggi/scripts/SingleCrystal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCrystal : MonoBehaviour, IChargeable, ILightSource {
+
+    public CrystalScript parent;
+
+    public void Charging()
+    {
+        parent.Charging();
+    }
+
+    public void UnCharging()
+    {
+        parent.UnCharging();
+    }
+
+    public float GetIntensity()
+    {
+        return 0;
+    }
+
+    public float GetRadius()
+    {
+        return 0;
+    }
+
+    public Transform GetTransform()
+    {
+        return this.gameObject.transform;
+    }
+
+    //When the crystals Light is shining on something
+    private void OnTriggerEnter(Collider other)
+    {
+        ILightTriggerable monster = other.gameObject.GetComponent(typeof(ILightTriggerable)) as ILightTriggerable;
+        if(monster != null)
+        {
+            monster.DetectingLightsource(this);
+        }
+       
+    }
+}
