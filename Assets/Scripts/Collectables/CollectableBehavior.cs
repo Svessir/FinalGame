8a68fa117ef32626d096b09edd845cd66aaff06d
sverrir <sverrir1993@gmail.com>
2017-03-16 22:12:46 +0000

    pull

---
+++ Assets/Scripts/Collectables/CollectableBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBehavior : MonoBehaviour 
+{
+	private bool isCollected = false;
+	public bool IsCollected {get{ return isCollected; }}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Collectables/CollectorManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectorManager : MonoBehaviour {
+
+	private static CollectorManager instance;
+	public static CollectorManager Instance { get { return instance; } }
+
+	private List<CollectableBehavior> collectables;
+
+	public int CollectablesFound 
+	{ 
+		get
+		{
+			int i = 0;
+			foreach (var collectable in collectables)
+				i += collectable.IsCollected ? 1 : 0;
+			return i;
+		} 
+	}
+
+	public void Awake() 
+	{
+		instance = this;
+	}
+
+	public void RegisterCollectable(CollectableBehavior collectable) 
+	{
+		collectables.Add (collectable);
+	}
+}
