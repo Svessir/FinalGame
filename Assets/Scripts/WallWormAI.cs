8be825f488eb83b6697b7024ff2bd7b0800bdd2f
Drulludanni <drulludanni5@gmail.com>
2017-03-19 16:26:37 +0000

    fish fix things

--- Assets/Scripts/WallWormAI.cs
+++ Assets/Scripts/WallWormAI.cs
@@ -43,7 +43,6 @@ public class WallWormAI : MonoBehaviour, ILightTriggerable {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(lights.Count);
         eye = eyeTransform.position;
         if (player == null) {
             Debug.Log("no player in scene");
