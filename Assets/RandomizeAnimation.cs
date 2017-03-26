99fe44edb63737ba0f4e48e0de986cf6a9be8f51
Drulludanni <gudni14@ru.is>
2017-03-26 17:28:21 +0000

    escape scene probably ready

---
+++ Assets/RandomizeAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var ani = GetComponent<Animator>();
+        ani.speed = Random.Range(2, 2.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
