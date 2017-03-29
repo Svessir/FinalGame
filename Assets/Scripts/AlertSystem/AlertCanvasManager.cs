657860d3fd125e95fb839578e41db9d06e3f2b5f
sverrir <sverrir1993@gmail.com>
2017-03-29 12:02:07 +0000

    skeleton for ui track elements

---
+++ Assets/Scripts/AlertSystem/AlertCanvasManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertCanvasManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject playerGameObject;
+
+	[SerializeField]
+	private float maximumDisplayRange = 20.0f;
+
+	[SerializeField]
+	private AlertUIElement alertUIPrefab;
+
+	private static List<GameObject> uiElements; 
+
+	private static AlertCanvasManager instance;
+
+	public static AlertCanvasManager Instance 
+	{ 
+		get
+		{ 
+			return instance;
+		}
+	}
+
+	public static void RegisterAlertable(IAlertable alertable) 
+	{
+		if (alertable != null && instance != null) {
+			instance.Register (alertable);
+		}
+	}
+
+	void Awake() 
+	{
+		instance = this;
+	}
+
+	private void Register(IAlertable alertable)
+	{
+		if (alertUIPrefab != null) 
+		{
+			AlertUIElement ui = Instantiate<AlertUIElement>(alertUIPrefab);
+			ui.transform.SetParent (transform, false);
+			ui.playerGameObject = playerGameObject;
+			ui.trackedAlertable = alertable;
+		}
+	}
+}
