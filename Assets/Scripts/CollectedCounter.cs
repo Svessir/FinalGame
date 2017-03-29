8027d609bce3fee208e5cd250c1f860d3de69f2c
Drulludanni <drulludanni5@gmail.com>
2017-03-29 14:15:33 +0000

    sometihng

---
+++ Assets/Scripts/CollectedCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectedCounter : MonoBehaviour {
+
+    int numberCollected = 0;
+    int totalNumber;
+
+	// Use this for initialization
+	void Start () {
+        CollectorManager.CollectedEvent += OnCollect;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollect(CollectableBehavior collected)
+    {
+        numberCollected = CollectorManager.Instance.CollectablesFound;
+        totalNumber = CollectorManager.Instance.TotalNumberOfCollectables;
+    }
+}
