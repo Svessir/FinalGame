40ec83d730b152a6176a0672eaeccdae9aeee264
Bollasupa <magnusvb14@ru.is>
2017-03-30 18:27:59 +0000

    added submarine bump sound to prefab

---
+++ Assets/Maggi/scripts/BumpSoundHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpSoundHandler : RespawnableBehavior {
+
+    public AudioSource audio;
+
+    private float deathTime;
+
+    private void Start()
+    {
+        deathTime = Time.time;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(PlayCrashSound());
+    }
+
+    IEnumerator PlayCrashSound()
+    {
+        yield return null;
+        if(Time.time - deathTime > 0.5f)
+        {
+            audio.Play();
+        }
+    }
+
+    public override void Respawn(Vector3 checkpointLocation)
+    {
+        deathTime = Time.time;
+    }
+}
