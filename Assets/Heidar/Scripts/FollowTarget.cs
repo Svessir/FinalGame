c9304b6c3e2e02eb7533a661fc1aafa368c3077f
geimkisi <geimkisi@gmail.com>
2017-03-31 15:03:32 +0000

    Camera zoom in the fish level

---
+++ Assets/Heidar/Scripts/FollowTarget.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 7.5f, 0f);
+
+	public float smoothTime = 0.3F;
+	private Vector3 velocity = Vector3.zero;
+
+	void FixedUpdate() {
+		Vector3 targetPosition = target.position + offset;
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
+
---
+++ Assets/Scripts/zoomCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomCamera : MonoBehaviour {
+
+	public FollowTarget currentCamera;
+
+	public bool enter;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			currentCamera.offset.z = -55;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Player")) {
+			currentCamera.offset.z = -35;
+		}
+	}
+}
